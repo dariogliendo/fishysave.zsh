9f05c7ed85a91db040e80121a633ccae01f74080
dliendo <dliendo@somoswoxi.com>
2025-06-24 10:07:30 -0300

    Initial fishysave

---
+++ README.md
@@ -0,0 +1,17 @@
+## fishysave
+
+Plugin to save and update functions and aliases directly from shell.
+
+## Install
+
+To install you have to add this plugin to your custom plugins folder located by default at ```$ZSH_CUSTOM```
+
+```bash
+git clone "$ZSH_CUSTOM/plugins/fishysave"
+```
+add fishysave to the plugins array of your zshrc file:
+```bash
+# ~/.zshrc
+plugins=(... fishysave)
+
+```
---
+++ fishysave.plugin.zsh
@@ -0,0 +1,42 @@
+
+local base_dir="${ZSH_SAVE_DIR:-${0:A:h}}"
+local alias_dir="$base_dir/aliases"
+local func_dir="$base_dir/functions"
+
+function save() {
+  local name="$1"
+  
+  if [[ -z "$name" ]]; then
+    echo "No parameter provided"
+    echo "Usage: save <alias or function>"
+    return 1
+  fi
+
+  mkdir -p "$alias_dir" "$func_dir"
+
+  if alias "$name" &>/dev/null; then
+    local alias_def
+    alias_def=$(alias "$name")
+
+    echo "alias $alias_def" > "$alias_dir/$name.zsh"
+    echo "Alias $name saved"
+  elif whence -w "$name" | grep -q function; then
+    local func_def
+    func_def=$(functions "$name")
+    echo "$func_def" > "$func_dir/$name.zsh"
+    echo "Function $name saved"
+  else
+    echo "Couldn't find a declared function or alias named '$name'"
+    return 2
+  fi
+
+  echo $base_dir
+}
+
+mkdir -p "$alias_dir" "$func_dir"
+
+setopt localoptions nullglob
+
+for file in "$base_dir"/aliases/*.zsh "$base_dir"/functions/*.zsh; do
+  [[ -f $file ]] && source "$file"
+done
